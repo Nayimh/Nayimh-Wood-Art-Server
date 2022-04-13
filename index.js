521b2fe305c4b04b8b316ee4bc6d0a284c60f482
Nayimh <hnaim6979@gmail.com>
2022-04-13 07:55:26 +0600

    test inprogress

--- index.js
+++ index.js
@@ -20,180 +20,168 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
     try {
-        await client.connect();
-        const database = client.db('WoodArt');
+      await client.connect();
+      const database = client.db("WoodArt");
 
-        const furnitureCollection = database.collection('furniture');
-        const orderCollection = database.collection('orders');
-        const reviewCollection = database.collection('reviews');
-        const usersCollection = database.collection('users');
+      const furnitureCollection = database.collection("furniture");
+      const orderCollection = database.collection("orders");
+      const reviewCollection = database.collection("reviews");
+      const usersCollection = database.collection("users");
 
-        // Furnitures section
-        // post 
-        app.post('/furniture', async (req, res) => {
-            const cursor = req.body;
-            const result = await furnitureCollection.insertOne(cursor);
-            res.send(result);
-        })
+      // Furnitures section
+      // post api
+      app.post("/furniture", async (req, res) => {
+        const cursor = req.body;
+        const result = await furnitureCollection.insertOne(cursor);
+        res.send(result);
+      });
 
-        // get 
-        app.get('/furniture', async (req, res) => {
-            const cursor = furnitureCollection.find({});
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+      // get
+      app.get("/furniture", async (req, res) => {
+        const cursor = furnitureCollection.find({});
+        const foods = await cursor.toArray();
+        res.send(foods);
+      });
 
-        // get by id
-        app.get('/furniture/:id', async (req, res) => {
-            const furnitureId = req.params.id;
-            const query = { _id: ObjectId(furnitureId) };
-            const furniture = await furnitureCollection.findOne(query);
-            res.send(furniture);
-        })
+      // single data
+      app.get("/furniture/:id", async (req, res) => {
+        const foodId = req.params.id;
+        const query = { _id: ObjectId(foodId) };
+        const food = await furnitureCollection.findOne(query);
+        res.send(food);
+      });
 
-        // update 
-        app.put('/furniture/:id', async (req, res) => {
-            const id = req.params.id;
-            const updateFurniture = req.body;
-            const filter = { _id: ObjectId(id) };
-            const option = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    furnitureName: updateFurniture?.name,
-                    description: updateFurniture?.desc,
-                    furnitureImage: updateFurniture?.img,
-                    madeBy: updateFurniture?.by,
-                    furniturePrice: updateFurniture?.price
+      // update
+      app.put("/furniture/:id", async (req, res) => {
+        const id = req.params.id;
+        const updateFurniture = req.body;
+        const filter = { _id: ObjectId(id) };
+        const option = { upsert: true };
+        const updateDoc = {
+          $set: {
+            furnitureName: updateFurniture?.name,
+            description: updateFurniture?.desc,
+            furnitureImage: updateFurniture?.img,
+            madeBy: updateFurniture?.by,
+            furniturePrice: updateFurniture?.price,
+          },
+        };
+        const result = furnitureCollection.updateOne(
+          filter,
+          option,
+          updateDoc
+        );
+        res.json(result);
+      });
 
-                },
-            };
-            const result = furnitureCollection.updateOne(filter, option, updateDoc);
-            res.json(result);
-        })
+      // delete
+      app.delete("/furniture/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const deleteFood = await furnitureCollection.deleteOne(query);
+        res.send(deleteFood);
+      });
 
-        // delete
-        app.delete('/furniture/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const deleteFurniture = await furnitureCollection.deleteOne(query);
-            res.json(deleteFurniture);
-        })
+      // Order section
 
-        // Order section
+      // get order by email
+      app.get("/orders/:email", async (req, res) => {
+        const email = req.params.email;
+        const singleOrder = orderCollection.find({});
+        const orders = await singleOrder.toArray();
+        const customerOrder = orders.filter(
+          (mail) => (mail.email = email)
+        );
+        res.send(customerOrder);
+      });
 
-         // get by email
-         app.get('/orders/:email', async (req, res) => {
-            const email = req.params.email;
-            const singleOrder = orderCollection.find({});
-            const order = await singleOrder.toArray();
-            const customerOrder =  order.filter(
-                (e) = (e?.email === email)
-            );
-            res.json(customerOrder);
-        });
+      // get Single Order
+      app.get("/orders/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { id: ObjectId(id) };
+        const order = await orderCollection.findOne(query);
+        res.json(order);
+      });
 
-        // post order
-        app.post('/orders', async (req, res) => {
-            const cursor = req.body;
-            const result = await orderCollection.insertOne(cursor);
-            res.json(result);
-        })
+      // post order
+      app.post("/orders", async (req, res) => {
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.json(result);
+      });
 
-        // get order
-        app.get('/orders', async (req, res) => {
-            const cursor = orderCollection.find({});
-            const result = await cursor.toArray();
-            res.json(result);
-        })
+      // delete specefic order
+      app.delete("/orders/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const deleteOrder = await orderCollection.deleteOne(query);
+        res.json(deleteOrder);
+      });
 
-       
-
-        // get by id
-        app.get('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const product = await orderCollection.findOne(query);
-            res.json(product);
-        })
-
-        // delete order
-        app.delete('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.json(result);
-            
-        })
-
-        // update Order
-        // app.put('/orders/:id', async (req, res) => {
-        //     const statusId = req.params.id;
-        //     const updateStatus = req.body;
-        //     const filter = { _id: ObjectId(statusId) };
-        //     const option = { upsert: true };
-        //     const updateOrder = {
-        //         $set: {
-        //             status: updateStatus.status
-        //         },
-        //     };
-        //     const accepOrder = await orderCollection.updateOne(filter, updateOrder, option);
-        //     res.json(accepOrder);
-        // })
-
-
-        // reviews section
-        // post ratings
+       // update Order
+       app.put("/orders/:id", async (req, res) => {
+        const statusId = req.params.id;
+        const updateStatus = req.body;
+        const filter = { _id: ObjectId(statusId) };
+        const option = { upsert: true };
+        const updateOrder = {
+          $set: {
+            status: updateStatus.status,
+          },
+        };
+        const acceptstatus = await orderCollection.updateOne(
+          filter,
+          updateOrder,
+          option
+        );
+        res.json(acceptstatus);
+       });
         
-        app.post('/ratings', async (req, res) =>{
-            const cursor = req.body;
-            const result = await reviewCollection.insertOne(cursor);
-            res.json(result);
-        })
+        
+      // post ratings
 
-        // get rating
+      app.post("/ratings", async (req, res) => {
+        const cursor = req.body;
+        const result = await reviewCollection.insertOne(cursor);
+        res.json(result);
+      });
 
-        app.get('/ratings', async (req, res) => {
-            const cursor = reviewCollection.find({});
-            const result = await cursor.toArray();
-            res.json(result);
-        })
+      // get rating
 
-        //get by email 
-        app.get('/ratings/:email', async (req, res) => {
-            const email = req.params.email;
-            const personRating = reviewCollection.find({});
-            const ratings = await personRating.toArray();
-            const userRatings = ratings.filter(mail => (mail?.email === email));
-            res.json(userRatings);
-        })
-
-        //get by id
-        app.get('/ratings/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const ratings = await reviewCollection.findOne(query);
-            res.json(ratings);
-        })
+      app.get("/ratings", async (req, res) => {
+        const cursor = reviewCollection.find({});
+        const result = await cursor.toArray();
+        res.json(result);
+      });
 
 
-        //delete ratings
-        app.delete('/ratings/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id : ObjectId(id) };
-            const deleteRating = await reviewCollection.deleteOne(query);
-            res.json(deleteRating);
-        })
+      //get by id
+      app.get("/ratings/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const ratings = await reviewCollection.findOne(query);
+        res.json(ratings);
+      });
 
+      //delete ratings
+      app.delete("/ratings/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const deleteRating = await reviewCollection.deleteOne(query);
+        res.json(deleteRating);
+      });
 
-        // userSection
-         // post user
+      // userSection
+      // post user
+      // post user
       app.post("/users", async (req, res) => {
         const user = req.body;
         const result = await usersCollection.insertOne(user);
         res.json(result);
       });
-      // upsert user api
-      app.put("/users", async (req, res) => {
+        
+       // upsert user api
+       app.put("/users", async (req, res) => {
         const user = req.body;
         const filter = { email: user.email };
         const option = { upsert: true };
@@ -204,7 +192,8 @@ async function run() {
           option
         );
         res.json(result);
-      });
+       });
+        
       // make admin user api
       app.put("/users/admin", async (req, res) => {
         const user = req.body;
@@ -228,9 +217,6 @@ async function run() {
         }
         res.json({ admin: isAdmin });
       });
-
-
-
     }
 
     finally {
